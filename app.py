5a7fbe74433249583a5a638275a00c9ab1a3f7d4
Kristof <kristofkakuszi@gmail.com>
2021-04-19 16:10:14 +0200

    LogDBRelationships

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ import random
 import sqlite3
 import string
 
+import passlib
 from flask import Flask, send_from_directory, request, jsonify, url_for, redirect
-from flask_login import LoginManager, UserMixin
-from flask_login import login_user, current_user
-from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin, login_user, current_user
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
+
 from forms import LoginForm
 
 UPLOAD_FOLDER = 'uploads'
@@ -17,6 +19,8 @@ UPLOAD_FOLDER = 'uploads'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 
 login_manager = LoginManager(app)
 
@@ -28,20 +32,29 @@ app.secret_key = 'hello'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True)
     password = db.Column(db.String(128))
+    #pictures = db.relationship('Images', backref='owner', lazy=True) #lehet lazy nem is kell
+
+    #one to many relationship egy usernek lehet több képe
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    def verify_password(self, password):
+        return check_password_hash(password, self.password)
 
 class Images(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(300))
     #data = db.Column(db.LargeBinary)
     fp = db.Column(db.String(264), unique=True)
+    #vmi = db.Column(db.valamiTipus, db.ForeignKey('user.id vagy token'), nullable=False) #lehet nullable nem is kell
 
 #db.create_all()
 #db.session.commit()
@@ -55,49 +68,34 @@ def index():
 def login():
     return send_from_directory('templates', 'index.html')
 
-
 @app.route('/onLogin', methods=['POST']) #loginPost
 def login_post():
 
-    #ezek tuti kellenek
-    log_object = request.get_json()
+    log_object = request.get_json() #ez biztos kell
     username = log_object['username']
     password = log_object['password']
 
+    #form = LoginForm()
+    #username = form.username.data
+    #password = form.password.data
+    #if form.validate_on_submit():
+
     user = User.query.filter_by(username=username).first()
 
-    #ide kell egy fgv ami megnezi a user valt segitsegevel hogy benne van-e + hash check
+    #if user and user.verify_password(password):
+    if user :
+        # ha a password megegyezik a db user passwordjával (ami hashes) akkor sikeres login
+        print("van ilyen felhasználó")
+        #login_user(user)
 
-    if not user:
-        return jsonify({'result': "nincs ilyen felhasználó"})
-
-
-
-    """
-    form = LoginForm()
-    if form.validate_on_submit():
-        conn = sqlite3.connect('app.db')
-        curs = conn.cursor()
-        curs.execute("SELECT * FROM user where username = (?)",)
-        user = list(curs.fetchone())
-        Us = load_user(user[0])
+        # valami miatt hibát dob
+    else:
+        print("nincs ilyen felhasználó vagy nem jól írtál be valamit")
+        return redirect(url_for('login')) #ez miatt nem megy át
+       #return jsonify({'result': "nincs ilyen felhasználó"})
 
 
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        #user = User.query.filter_by(username=username).first()
-        #if not user or not check_password_hash(user.password, password):
-        #if user is None or not check_password_hash(user.password, password):
-        if user is None or not user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            print("nem jok az adatok")
-        else:
-            print("jok az adatok")
-    #login_user(user)
 
-
-    """
 """
     if(): #ha a login okes -> gen token
 
@@ -109,7 +107,6 @@ def login_post():
         for number in range(length_of_string):
             token += random.choice(letters_and_digits)
         #print(token)
-
 
         token = User(token=token)
         user = User.query.filter_by(username=username).first()
@@ -134,7 +131,7 @@ def new_user():
     user = User.query.filter_by(username=username).first()
     if user:
         print("letezik ilyen")
-        return redirect(url_for('register'))                                     # nem mukodik viszont ez miatt nem megy at loginra
+        return redirect(url_for('register')) # nem mukodik viszont ez miatt nem megy at loginra
         #return jsonify({'result': "sikeres reg"})
     else:
         db.session.add(signup)
--- forms.py
+++ forms.py
@@ -1,9 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField
 import sqlite3
 
 class LoginForm(FlaskForm):
  username = StringField('username')
  password = PasswordField('password')
- submit = SubmitField('Login')
+
